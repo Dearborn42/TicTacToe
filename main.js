fad41960175a09711dfaed5c8fb7d5dfe3099758
AndrewMurphy <amurf26@outlook.com>
2022-11-04 18:23:31 -0700

    Changed project to connect 4

--- main.js
+++ main.js
@@ -1,44 +1,33 @@
-let canvas = document.getElementById("canvas");
-let paint = canvas.getContext("2d");
-// Draws lines in even intervals on canvas, twice to make lines thicker
-function draw(){
-    paint.beginPath();
-    for(let i=0; i<2; i++){
-        for(let i=0; i<500; i+=100){
-        paint.moveTo(i, 0);
-        paint.lineTo(i, 500);
-    }
-    for(let i=0; i<400; i+=50){
-        paint.moveTo(0, i);
-        paint.lineTo(500, i);
-    }
-    }
-    paint.stroke();
-}
-draw();
+"use strict";
+let turn = 1;
+
 
 let board = [
-    [null, null, null], 
-    [null, null, null],
-    [null, null, null]
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null],
+    [null, null, null, null, null]
 ]
-function moves(event){
-    let x = event.clientX;
-        if(x <= 829){
-        x = 0;
-    }else if(x <= 1085){
-        x = 1;
-    }else{
-        x = 2;
-    }
 
-    let y = event.clientY;
-    // if(y <= 329){
-    //     y = 0;
-    // }else if(y <= 559){
-    //     y = 1;
-    // }else{
-    //     y = 2;
-    // }
-    console.log(x, y);
+
+function moves(action){
+        if(turn%2 != 0){
+            chose(action, 'r');
+        }else{
+            chose(action, 'y');
+        }
+    turn++;
+}
+
+let chose = function(action, string){
+    for(let i=0; i<5; i++){
+        if(board[action][i]){}
+    }
+    if(board[action] == null){
+        board[action] = string;
+        turn--;
+    }
+    console.log(i, board);
 }
