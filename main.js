8163cbf6c10209eac726ceb062d21568481b73c5
Dearborn42 <amurf26@outlook.com>
2022-11-07 08:54:17 -0700

    Remaking check functions

--- main.js
+++ main.js
@@ -31,22 +31,24 @@ let board = [
 function moves(action) {
     if (turn % 2 != 0) {
         chose(action, 'r', 'red');
-        win('r');
+        vWin('r', action);
+        // hWin('r');
     } else {
         chose(action, 'y', 'yellow');
-        win('y');
+        vWin('y', action);
+        // hWin('y');
     }
     turn++;
 }
 
 let chose = function (action, string, color) {
-    let a = action.split("");
+    let a = action;
     for (let i = 0; i < 5; i++) {
-        if (board[a[0]][i] == null) {
-            board[a[0]][i] = string;
+        if (board[a][i] == null) {
+            board[a][i] = string;
             ctx.beginPath();
             ctx.fillStyle = color;
-            ctx.arc(((a[0] * 51) + 22), (((4 - i) * 30) + 15), 12.5, 0, 2 * Math.PI);
+            ctx.arc(((a * 51) + 22), (((4 - i) * 30) + 15), 12.5, 0, 2 * Math.PI);
             ctx.fill();
             ctx.stroke();
             i += 5;
@@ -54,10 +56,25 @@ let chose = function (action, string, color) {
     }
 }
 
+let vWin = function(String, action) {
+    let a = action;
+    let check = 0;
+    let filter = board[a].filter(check => check);
+    for(let i = 1; i < 5; i++){
+        if(filter[i] == String){
+            check++;
+        }
+    }
+}
+
+let hWin = function(String) {
+    let check2 = 0;
+
+}
+
+
 
 let win = function (string) {
-    let check1 = 0;
-    let check2 = 0;
     let check3 = 0;
     let check4 = 0;
     let r = 0;
@@ -73,69 +90,38 @@ let win = function (string) {
     //    r [null, null, null, null, null],
     //    r [null, null, null, null, null]
     //      ]
-    for(let r=0; r<6; r++){
-        for(let c=0; c<5; c++){
-            if(board[r][c] === string){
-                check1++;
-                if(board[r][c+1] != string){
-                    check1--;
-                }
-            }
-        }
-        if(check1 === 3){
-            console.log(string +' wins');
-        }
-    }
-    for(let c=0; c<5; c++){
-        for(let r=0; r<6; r++){
-            if(board[r][c] === string){
-                check2++;
-                if(c != 4){
-                    if(board[r][c+1] != string){
-                        check2--;
-                    }
-                } 
-            }
-        }
-        if(check2 === 3){
-            console.log(string +' wins');
-        }
-    }
 
-    for (let c = 5; c>0; c--) {
-        for (let r = 0; r < 6; r++) {
-            if (board[r][c] === string) {
-                c--;
-                check3++;
-                if( r != 0){
-                    if(board[r-1][c] != string){
-                        check3--;
-                    }else{
-                        check3++;
-                    }
-                }
-            }
-            if(check3 === 4){
-                console.log(string +' wins');
-            }
-        }
-        console.log(check3, string);
-    }
+    // for(let c=0; c<5; c++){
+    //     for(let r=0; r<6; r++){
+    //         if(board[r][c] === string){
+    //             check2++;
+    //             if(c != 4){
+    //                 if(board[r][c+1] != string){
+    //                     check2--;
+    //                 }
+    //             } 
+    //         }
+    //     }
+    //     if(check2 === 3){
+    //         console.log(string +' wins');
+    //     }
+    // }
 
-   for (let c = 4; c>0; c--) {
-        for (let r = 0; r < 6; r++) {
-            if (board[r][c] === string) {
-                c--;
-                check4++;
-                if( r != 5){
-                    if(board[r+1][c] != string){
-                        check4--;
-                    }
-                }
-            }
-            if(check4 === 4){
-                console.log(string +' wins');
-            }
-        }
-    }
+
+//    for (let c = 4; c>0; c--) {
+//         for (let r = 0; r < 6; r++) {
+//             if (board[r][c] === string) {
+//                 c--;
+//                 check4++;
+//                 if( r != 5){
+//                     if(board[r+1][c] != string){
+//                         check4--;
+//                     }
+//                 }
+//             }
+//             if(check4 === 4){
+//                 console.log(string +' wins');
+//             }
+//         }
+//     }
 }
