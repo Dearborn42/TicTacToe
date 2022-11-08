438eea51313fc51d6b3600922f51afe5904a6f06
AndrewMurphy <amurf26@outlook.com>
2022-11-07 17:51:06 -0700

    Update main.js

--- main.js
+++ main.js
@@ -32,20 +32,20 @@ function moves(action) {
     if (turn % 2 != 0) {
         chose(action, 'r', 'red');
         vWin('r', action);
-        // hWin('r');
+        hWin('r', action);
     } else {
         chose(action, 'y', 'yellow');
         vWin('y', action);
-        // hWin('y');
+        hWin('y', action);
     }
     turn++;
 }
 
-let chose = function (action, string, color) {
+let chose = function (action, String, color) {
     let a = action;
     for (let i = 0; i < 5; i++) {
         if (board[a][i] == null) {
-            board[a][i] = string;
+            board[a][i] = String;
             ctx.beginPath();
             ctx.fillStyle = color;
             ctx.arc(((a * 51) + 22), (((4 - i) * 30) + 15), 12.5, 0, 2 * Math.PI);
@@ -60,21 +60,25 @@ let vWin = function(String, action) {
     let a = action;
     let check = 0;
     let filter = board[a].filter(check => check);
-    for(let i = 1; i < 5; i++){
-        if(filter[i] == String){
+    for(let i = 0; i < 5; i++) {
+        if(filter[i] == String && filter[i] == filter[i + 1]){
             check++;
         }
     }
+    if(check == 3){
+        console.log(String + ' wins');
+    }
 }
 
-let hWin = function(String) {
+let hWin = function(String, action) {
     let check2 = 0;
-
+    let a = action;
+    let filter = board[a].filter(check => check);
 }
 
 
 
-let win = function (string) {
+let win = function (String) {
     let check3 = 0;
     let check4 = 0;
     let r = 0;
@@ -93,35 +97,41 @@ let win = function (string) {
 
     // for(let c=0; c<5; c++){
     //     for(let r=0; r<6; r++){
-    //         if(board[r][c] === string){
+    //         if(board[r][c] === String){
     //             check2++;
     //             if(c != 4){
-    //                 if(board[r][c+1] != string){
+    //                 if(board[r][c+1] != String){
     //                     check2--;
     //                 }
     //             } 
     //         }
     //     }
     //     if(check2 === 3){
-    //         console.log(string +' wins');
+    //         console.log(String +' wins');
     //     }
     // }
 
 
 //    for (let c = 4; c>0; c--) {
 //         for (let r = 0; r < 6; r++) {
-//             if (board[r][c] === string) {
+//             if (board[r][c] === String) {
 //                 c--;
 //                 check4++;
 //                 if( r != 5){
-//                     if(board[r+1][c] != string){
+//                     if(board[r+1][c] != String){
 //                         check4--;
 //                     }
 //                 }
 //             }
 //             if(check4 === 4){
-//                 console.log(string +' wins');
+//                 console.log(String +' wins');
 //             }
 //         }
 //     }
 }
+
+
+
+
+
+
