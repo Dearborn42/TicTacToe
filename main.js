fd08401dc7316c288d5d879737a1f3aea86fd900
Andrew <324737364@student.guhsdaz.org>
2022-11-08 00:39:36 -0700

    Finished vertical check

--- main.js
+++ main.js
@@ -26,6 +26,14 @@ let board = [
     [null, null, null, null, null],
     [null, null, null, null, null],
     [null, null, null, null, null]
+];
+
+let board2 = [
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null],
+    [null, null, null, null, null, null]
 ]
 
 function moves(action) {
@@ -46,6 +54,7 @@ let chose = function (action, String, color) {
     for (let i = 0; i < 5; i++) {
         if (board[a][i] == null) {
             board[a][i] = String;
+            board2[i][a] = String;
             ctx.beginPath();
             ctx.fillStyle = color;
             ctx.arc(((a * 51) + 22), (((4 - i) * 30) + 15), 12.5, 0, 2 * Math.PI);
@@ -59,7 +68,7 @@ let chose = function (action, String, color) {
 let vWin = function(String, action) {
     let a = action;
     let check = 0;
-    let filter = board[a].filter(check => check);
+    let filter = board[a].map(check => check);
     for(let i = 0; i < 5; i++) {
         if(filter[i] == String && filter[i] == filter[i + 1]){
             check++;
@@ -70,68 +79,18 @@ let vWin = function(String, action) {
     }
 }
 
+// does not work
 let hWin = function(String, action) {
+    let a = parseInt(action);
+    let x = 0;
+    let check = 0;
     let check2 = 0;
-    let a = action;
-    let filter = board[a].filter(check => check);
+
+    for(let i=0; i<5; i++){
+        let row = board2[i].map(check => check);
+        let nRow = row.filter(check => check);
+        console.log(nRow)
+    }
 }
 
-
-
-let win = function (String) {
-    let check3 = 0;
-    let check4 = 0;
-    let r = 0;
-    let c = 0;
-    // r maxes out at 6
-    // c maxes out at 5
-    //     let board = [
-    //    c     c     c     c     c
-    //    r [null, null, null, null, null],
-    //    r [null, null, null, null, null],
-    //    r [null, null, null, null, null],
-    //    r [null, null, null, null, null],
-    //    r [null, null, null, null, null],
-    //    r [null, null, null, null, null]
-    //      ]
-
-    // for(let c=0; c<5; c++){
-    //     for(let r=0; r<6; r++){
-    //         if(board[r][c] === String){
-    //             check2++;
-    //             if(c != 4){
-    //                 if(board[r][c+1] != String){
-    //                     check2--;
-    //                 }
-    //             } 
-    //         }
-    //     }
-    //     if(check2 === 3){
-    //         console.log(String +' wins');
-    //     }
-    // }
-
-
-//    for (let c = 4; c>0; c--) {
-//         for (let r = 0; r < 6; r++) {
-//             if (board[r][c] === String) {
-//                 c--;
-//                 check4++;
-//                 if( r != 5){
-//                     if(board[r+1][c] != String){
-//                         check4--;
-//                     }
-//                 }
-//             }
-//             if(check4 === 4){
-//                 console.log(String +' wins');
-//             }
-//         }
-//     }
-}
-
-
-
-
-
-
+let dWin = function(String, action) {}
